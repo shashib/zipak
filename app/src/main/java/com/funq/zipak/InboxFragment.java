e7584f3a003fda85c65299c4b686b205418ccbb3
Shashi Singh <shashi.bhushan@gmail.com>
2014-09-04 01:02:47 -0700

    added fragment structure

---
+++ app/src/main/java/com/funq/zipak/InboxFragment.java
@@ -0,0 +1,20 @@
+package com.funq.zipak;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by soham on 9/4/14.
+ */
+public class InboxFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+
+        return rootView;
+    }
+}
