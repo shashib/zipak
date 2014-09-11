303f15c12480bd89426edbe1082eabfe8187b7e6
Shashi Singh <shashi@theklipp.com>
2014-09-10 22:26:13 -0700

    Hiding action bar from login and signup screen

--- app/src/main/java/com/funq/zipak/SignUpActivity.java
+++ app/src/main/java/com/funq/zipak/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.funq.zipak;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -28,6 +29,9 @@ public class SignUpActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_sign_up);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
         mUsername = (EditText) findViewById(R.id.usernameField);
         mPassword = (EditText) findViewById(R.id.passwordField);
